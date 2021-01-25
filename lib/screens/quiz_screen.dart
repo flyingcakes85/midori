7fa6ec8622fadcd1d024c6601fa5782dcffb79c4
Snehit Sah <snehitsah@protonmail.com>
2021-01-25 18:40:04 +0530

    Prevent Accidental Exit

--- lib/screens/quiz_screen.dart
+++ lib/screens/quiz_screen.dart
@@ -19,13 +19,39 @@ class _QuizScreenState extends State<QuizScreen> {
   Widget build(BuildContext context) {
     final QuizArguments args = ModalRoute.of(context).settings.arguments;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Quiz'),
-      ),
-      body: Center(
-        child: Text(args.quizContents.toString()),
-      ),
-    );
+    return WillPopScope(
+        onWillPop: _onBackPressed,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Quiz'),
+          ),
+          body: Center(
+            child: Text(args.quizContents.toString()),
+          ),
+        ));
+  }
+
+  Future<bool> _onBackPressed() {
+    return showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            actionsPadding: EdgeInsets.only(right: 15, bottom: 15),
+            title: new Text('Really abort quiz?'),
+            content: new Text(
+                'This will abort the quiz and current scoers will not be added to the total.'),
+            actions: <Widget>[
+              GestureDetector(
+                onTap: () => Navigator.of(context).pop(false),
+                child: Text("NO"),
+              ),
+              SizedBox(height: 16, width: 10),
+              GestureDetector(
+                onTap: () => Navigator.of(context).pop(true),
+                child: Text("YES"),
+              ),
+            ],
+          ),
+        ) ??
+        false;
   }
 }
